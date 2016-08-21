20fb55622477e724a41a913000e05ba0e9218e97
mbax4jp3 <jean-francois.pascal@student.manchester.ac.uk>
2016-08-21 17:42:03 +0300

    Added server-3.js

--- NodeJS/Week1 node basics/node-http/server-2.js
+++ NodeJS/Week1 node basics/node-http/server-2.js
@@ -1,3 +1,4 @@
+//import needed modules
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
@@ -5,39 +6,51 @@ var path = require('path');
 var hostname = 'localhost';
 var port = 3000;
 
+//create server
 var server = http.createServer(function(req, res){
-  console.log('Request for ' + req.url + ' by method ' + req.method);
-  if (req.method == 'GET') {
-    var fileUrl;
+    //print out the request
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+
+    //we only handle get
+    if (req.method == 'GET') {  
+
         if (req.url == '/') fileUrl = '/index.html';
-    else fileUrl = req.url;
+            else fileUrl = req.url;
+
         var filePath = path.resolve('./public'+fileUrl);
         var fileExt = path.extname(filePath);
-        if (fileExt == '.html') {
-       fs.exists(filePath, function(exists) {                 if (!exists) {
-        	res.writeHead(404, { 'Content-Type': 'text/html' });
-        	res.end('<html><body><h1>Error 404: ' + fileUrl + 
-                        ' not found</h1></body></html>');
-        	return;
-          }
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-          fs.createReadStream(filePath).pipe(res);
-              });
-    }
-    else {
 
+        // if the file extension is html
+        if (fileExt == '.html') {
+            // the exists variable in the function will have true if the file exists
+            fs.exists(filePath, function(exists) {
+                //in case it doesn't 
+                if (!exists) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end('<html><body><h1>Error 404: ' + fileUrl + ' not found</h1></body></html>');
+                    return;
+                }
+
+                //if it does exist
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                //read the file and then pipe it to the response message
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } //end if (fileExt == '.html')
+        //i'm only handling html files
+        else {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('<html><body><h1>Error 404: ' + fileUrl +  ' not a HTML file</h1></body></html>');
+        }  
+    }//end if (req.method == 'GET')
+    // anything else other than get it doesn't work
+    else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<html><body><h1>Error 404: ' + fileUrl + 
-                ' not a HTML file</h1></body></html>');
+        res.end('<html><body><h1>Error 404: ' + req.method + ' not supported</h1></body></html>');
     }
-  }
-  else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<html><body><h1>Error 404: ' + req.method + 
-                ' not supported</h1></body></html>');
-  }
 })
 
+//start server
 server.listen(port, hostname, function(){
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
